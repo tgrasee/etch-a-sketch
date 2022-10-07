cce80f4fecb246f208835ee5f8536829970beb38
tgrasee <tgrasee@gmail.com>
2022-10-07 17:49:36 -0400

    Add event listener to each div with mouseover event to change background color.

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const container = document.getElementById("grid");
 
 function makeGrid(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
-        let cell = document.createElement("div");
-        cell.innerText = "1";
-        container.appendChild(cell).className = "grid-item";
+      let div = document.createElement("div");
+    //   cell.innerText = "1";
+      container.appendChild(div).className = "grid-item";
+      let cell = document.getElementById("grid-item");
+      div.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = 'lightgray';
+        });
     };
 };
 
