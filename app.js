52a884cfa74c93bcea9177eeef05058519962e77
tgrasee <tgrasee@gmail.com>
2022-10-08 09:54:55 -0400

    Remove cell variable and cell innertext.

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const container = document.getElementById("grid");
 
+
 function makeGrid(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
       let div = document.createElement("div");
-    //   cell.innerText = "1";
       container.appendChild(div).className = "grid-item";
-      let cell = document.getElementById("grid-item");
       div.addEventListener("mouseover", (event) => {
         event.target.style.backgroundColor = 'lightgray';
         });
