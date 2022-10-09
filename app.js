e683db1a296ff16b42662797cb3a3888ae7d95ab
tgrasee <tgrasee@gmail.com>
2022-10-09 10:28:19 -0400

    Add event listener to gridSize button. Add function sizePrompt.

--- app.js
+++ app.js
@@ -15,6 +15,27 @@ function makeGrid(rows, cols) {
   clearBtn.addEventListener("click", () => {
     window.location.reload();
     });
+
+  let gridSize = document.getElementById("gridSize");
+  gridSize.addEventListener("click", () => {
+    sizePrompt();
+  });
+}
+
+function sizePrompt() {
+  let row = prompt("Please enter how many rows for the new grid", "16");
+  let column = prompt("Please enter how many columns for the new grid", "16");
+  console.log(isNaN(row));
+  while (isNaN(row) === true || isNaN(column) === true) {
+    if (isNaN(row) === true) {
+      row = prompt("Please a number for rows", "16");
+    }
+    else if (isNaN(column) === true) {
+      column = prompt("Please a number for columns", "16");
+    }
+  }
+  console.log(row);
+  console.log(column);
 }
 
 makeGrid(16, 16);
