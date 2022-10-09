b5de12423319a4801ad3a23285ceb91a613a4e5d
tgrasee <tgrasee@gmail.com>
2022-10-09 09:13:20 -0400

    Add clearBtn logic.

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const container = document.getElementById("grid");
 
-
 function makeGrid(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
-      let div = document.createElement("div");
-      container.appendChild(div).className = "grid-item";
-      div.addEventListener("mouseover", (event) => {
-        event.target.style.backgroundColor = 'lightgray';
-        });
-    };
-};
+  for (c = 0; c < (rows * cols); c++) {
+    let div = document.createElement("div");
+    container.appendChild(div).className = "grid-item";
+    div.addEventListener("mouseover", (event) => {
+      event.target.style.backgroundColor = 'lightgray';
+      });
+  };
+
+  let clearBtn = document.getElementById("clearBtn");
+  clearBtn.addEventListener("click", () => {
+    window.location.reload();
+    });
+}
 
 makeGrid(16, 16);
